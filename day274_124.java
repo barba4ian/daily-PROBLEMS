c3ea40420eb5d140f1731369867eb8ef30387470
barb4rian <aat.sambhavrocksingh123@gmail.com>
2026-05-19 20:41:38 +0530

    day274_124.java

---
+++ day274_124.java
@@ -0,0 +1,20 @@
+class Solution {
+        
+        int ans = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+
+    public int dfs(TreeNode root) {
+        if (root == null)
+            return 0;
+        
+        int l = Math.max(0, dfs(root.left));
+        int r = Math.max(0, dfs(root.right));
+
+        ans = Math.max(ans, root.val + l + r);
+
+        return root.val + Math.max(l, r);
+    } 
+}
